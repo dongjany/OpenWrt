5bf45307359e0019f52b17e44a24e48e48254f35
dongjany <3145255636@qq.com>
2026-09-09 21:21:24 +0800

    Update target.mk

--- include/target.mk
+++ include/target.mk
@@ -26,9 +26,8 @@ DEFAULT_PACKAGES:=\
 	uclient-fetch \
 	urandom-seed \
 	luci luci-compat wget-ssl curl ca-certificates htop \
-	default-settings luci-app-upnp miniupnpd-iptables luci-app-wol luci-app-vlmcsd luci-app-ramfree \
+	default-settings luci-app-upnp miniupnpd-iptables bind-host luci-app-ramfree \
 	luci-app-ddns ddns-scripts-cloudflare ddns-scripts_aliyun ddns-scripts_dnspod \
-	luci-app-timecontrol luci-app-control-timewol luci-app-control-webrestriction luci-app-control-weburl \
 	urngd
 
 ifneq ($(CONFIG_SELINUX),)
